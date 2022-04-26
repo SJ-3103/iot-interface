d872525e0d286f6e5924095e1eb74b001cffcbec
Rishabh Jain <jrishabh76@gmail.com>
2022-04-26 22:48:43 +0530

    fix: early rendering of component before state loads

--- app-ui/src/components/MyChart.jsx
+++ app-ui/src/components/MyChart.jsx
@@ -1,108 +1,99 @@
-import { useEffect, useState, useMemo } from "react"
-import "../res/chart.css"
+import { useEffect, useState } from "react";
+import "../res/chart.css";
 
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js'
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-import { Line } from 'react-chartjs-2'
+import { Line } from "react-chartjs-2";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-)
-
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'bottom',
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "bottom",
+    },
+    title: {
+      display: true,
+      text: "Line Chart",
+    },
+  },
+};
+
+export default function MyChart(props) {
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "",
+        borderColor: "",
+        backgroundColor: "",
+        data: [],
       },
-      title: {
-        display: true,
-        text: 'Line Chart',
-      }
-    }
-}
+    ],
+  });
 
+  useEffect(() => {
+    //   removed non required function wrapper to update the state
+    setChartData({
+      labels: props.data.map((val) => val["date"]),
+    });
 
-export default function MyChart(props) {    
-
-    const [chartData,setChartData] = useState({ 
-        labels: [], 
+    setChartData((prevState) => {
+      return {
+        ...prevState,
         datasets: [
-            {
-                label: "",
-                borderColor: "",
-                backgroundColor: "",   
-                data: []
-            }
-        ]
-    })
-    
-    useEffect(() => {
+          {
+            label: "Temperature",
+            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+            data: props.data.map((val) => val["temperature"]),
+          },
+          {
+            label: "Humidity",
+            borderColor: "rgb(53, 162, 235)",
+            backgroundColor: "rgba(53, 162, 235, 0.5)",
+            data: props.data.map((val) => val["humidity"]),
+          },
+          {
+            label: "Light Intensity",
+            borderColor: "rgb(53, 162, 100)",
+            backgroundColor: "rgba(53, 162, 100, 0.5)",
+            data: props.data.map((val) => val["lightval"]),
+          },
+          {
+            label: "Moisture Value",
+            borderColor: "rgb(153, 62, 35)",
+            backgroundColor: "rgba(153, 62, 35, 0.5)",
+            data: props.data.map((val) => val["moisture"]),
+          },
+        ],
+      };
+    });
+  }, []);
 
-        const myfunction = () => {
+  return (
+    <div className="chart">
+      <h3>Line Chart of Different Parameters</h3>
 
-            setChartData({
-                labels: props.data.map((val) => val["date"])
-            })
-
-            setChartData((prevState) => {
-                return ({
-                    ...prevState,
-                    datasets: [
-                        {
-                            label: 'Temperature',
-                            borderColor: 'rgb(255, 99, 132)',
-                            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                            data: props.data.map((val) => val["temperature"])
-                        },
-                        {
-                            label: 'Humidity',
-                            borderColor: 'rgb(53, 162, 235)',
-                            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-                            data: props.data.map((val) => val["humidity"])
-                        },
-                        {
-                            label: 'Light Intensity',
-                            borderColor: 'rgb(53, 162, 100)',
-                            backgroundColor: 'rgba(53, 162, 100, 0.5)',
-                            data: props.data.map((val) => val["lightval"])
-                        },
-                        {  
-                            label: 'Moisture Value',
-                            borderColor: 'rgb(153, 62, 35)',
-                            backgroundColor: 'rgba(153, 62, 35, 0.5)',
-                            data: props.data.map((val) => val["moisture"])
-                        }
-                    ]
-                })
-            })
-        
-        }
-        myfunction()  
-    },[])
-
-
-    return (
-        <div className="chart">
-            <h3>Line Chart of Different Parameters</h3>
-
-            <Line options={options} data={chartData}/>
-        </div>
-    )
+      <Line options={options} data={chartData} />
+    </div>
+  );
 }

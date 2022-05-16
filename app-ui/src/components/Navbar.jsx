f7765c2335f315dadf468d12c7fb4b63836b3f42
Shubham Jain <shbhm89300@gmail.com>
2022-05-16 19:33:00 +0530

    about page v0.1

--- app-ui/src/components/Navbar.jsx
+++ app-ui/src/components/Navbar.jsx
@@ -2,24 +2,31 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 import "../res/navbar.css"
 
-export default function Navbar() {
-    const [number,setNumber] = useState(3)
-  
+export default function Navbar(props) {
+    const [number,setNumber] = useState()
+
     return(
       <header className='header'>
         <nav>
+          <Link to='/'>
+            <ul>
+              <div id="nav-image"></div>
+              <h3>Growth Monitoring of Plants using Raspberry Pi and IoT</h3>
+            </ul>
+          </Link>
+          
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
-          </ul>
-          <ul><div></div></ul>
-          <ul>
             <li><Link to="/images">Images</Link></li>
-            <li id="mail-box"><Link to="/emails">E-Mails <span>{number}</span> </Link></li>
+            <li id="mail-box">
+              <Link to="/emails">E-Mails 
+                {number ? (<span>{number}</span>) : (console.log("Nothing")) }
+              </Link>
+            </li>
           </ul>
         </nav>
   
-        <h2>Growth Monitoring of Plants using Raspberry Pi and IoT</h2>
       </header>
     )
 }
